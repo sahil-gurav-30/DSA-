018d95c13beb370a724153977bbe201cc81ad747
sahil-gurav-30 <psahilp.24.beai@acharya.ac.in>
2026-03-12 10:14:30 +0530

    Update Linear_Search.py

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,3 +1,5 @@
+#Finding an element using linear search 
+
 def linear_search(arr,key):
     for i in range(len(arr)):
         if arr[i]==key:
@@ -10,3 +12,5 @@ if result!=-1:
     print("Element found at index",result)
 else:
     print("Element not found")
+
+#Time complexity O(n)
